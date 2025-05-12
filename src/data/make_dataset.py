c7b02f383cd908745f8cf4e14d84e87b99a3dd13
T01d4 <casinos_cover1s@icloud.com>
2025-05-12 11:20:05 +0200

    comit

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,11 +3,13 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
 
 
-@click.command()
+"""@click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
+"""
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -26,5 +28,5 @@ if __name__ == '__main__':
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
-
-    main()
+    print(os.getcwd())
+    main('/data/raw', '/data/processed')
